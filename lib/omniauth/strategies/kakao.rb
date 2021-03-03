9c9a7576e9c45aa582bb30ecb08fbe96a4cc7d00
woongjae lee <winzett0@gmail.com>
2021-03-03 15:21:26 +0900

    kakao account 추가

--- lib/omniauth/strategies/kakao.rb
+++ lib/omniauth/strategies/kakao.rb
@@ -17,7 +17,20 @@ module OmniAuth
       info do
         {
           'name' => raw_properties&.dig('nickname'),
+          'profile' => raw_properties&.dig('profile_image'),
           'image' => raw_properties&.dig('thumbnail_image'),
+        }
+      end
+      
+      kakao_account do
+        {
+          'profile' => raw_kakao_account&.dig('profile'),
+          'email' => raw_kakao_account&.dig('email'),
+          'age_range' => raw_kakao_account&.dig('age_range'),
+          'birthday' => raw_kakao_account&.dig('birthday'),
+          'gender' => raw_kakao_account&.dig('gender'),
+          'phone_number' => raw_kakao_account&.dig('phone_number'),
+
         }
       end
 
@@ -33,6 +46,11 @@ module OmniAuth
       def raw_properties
         @raw_properties ||= raw_info.dig('properties')
       end
+
+      def raw_kakao_account
+        @raw_properties ||= raw_info.dig('kakao_account')
+      end
+      
     end
   end
 end
